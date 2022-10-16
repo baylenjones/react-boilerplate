4a9bc8078feef25ad15c8cfeb477bde0f5183b29
baylenjones <baylen.jones0224@gmail.com>
2022-10-15 22:32:02 -0500

    test

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {MenuIcon, XIcon} from '@heroicons/react/outline'
+
+const Navbar = () => {
+  return (
+    <div className='w-screen h-20'>
+        <MenuIcon classname='w-5' />
+    </div>
+  )
+}
+
+export default Navbar
